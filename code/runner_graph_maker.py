b48494205c8f0b2ac41b2b1ebc2cb3ac68da9438
Kyle McMillan <kcm40@cam.ac.uk>
2025-02-17 13:28:07 +0000

    feat: graphs

--- code/runner_graph_maker.py
+++ code/runner_graph_maker.py
@@ -12,10 +12,16 @@ if __name__ == '__main__':
     runner_individual_no_bias = r"C:\Users\Kyle\Desktop\Results-Computation-In-Biological-NNs\results_runner\runner_individual_no_bias\0"
     runner_different_y_ind_v_diff_lr = r"C:\Users\Kyle\Desktop\Results-Computation-In-Biological-NNs\results_runner\runner_different_y_ind_v_diff_lr\0"
     runner_super_varied = r"C:\Users\Kyle\Desktop\Results-Computation-In-Biological-NNs\results_runner\runner_super_varied\0"
+    runner_y0_3_extra_long_500 = r"C:\Users\Kyle\Desktop\Results-Computation-In-Biological-NNs\results_runner\runner_y0_3_extra_long_500\0"
+    runner_y0_3_extra_long800 = r"C:\Users\Kyle\Desktop\Results-Computation-In-Biological-NNs\results_runner\runner_y0_3_extra_long_800\0"
+    runner_rosenbaum_50 = r"C:\Users\Kyle\Desktop\Results-Computation-In-Biological-NNs\results_runner\runner_rosenbaum_50\0"
+    runner_y0_4_extra_long_100 = r"C:\Users\Kyle\Desktop\Results-Computation-In-Biological-NNs\results_runner\runner_y0_4_extra_long_100\0"
+    runner_y0_4_extra_long_120 = r"C:\Users\Kyle\Desktop\Results-Computation-In-Biological-NNs\results_runner\runner_y0_4_extra_long_120\1"
 
-    runner_folders = [runner_diff_y0_3_chems, runner_individual_no_bias, runner_different_y_ind_v_diff_lr, runner_super_varied]
-    labels = [r"$\tau_{min}=2$", r"Layer Dependent $\tau_{min}=1$", r"Layer Dependent $\tau_{min}=2$", r"$\tau_{min}=2$ Meta Learner Changed"]
-    colors = ["blue", "fuchsia", "lime", "red"]
+
+    runner_folders = [runner_super_varied, runner_y0_3_extra_long800, runner_rosenbaum_50]
+    labels = [r"$\tau_{max}=50$", r"$\tau_{max}=100$", r"rosenbaum"]
+    colors = ["red", "blue", "fuchsia", "lime"]
 
     plt.rc('font', family='serif', size=14)
     plt.figure(figsize=(13, 8))
@@ -23,24 +29,32 @@ if __name__ == '__main__':
         all_files = os.listdir(i)
         all_files_int = [int(all_files[i] ) for i in range(len(all_files))]
         all_files_int.sort()
+        """if index != 2:
+            all_files_int.remove(225)
+            all_files_int.remove(275)
+            all_files_int.remove(325)"""
+
         all_files = [str(x) for x in all_files_int]
         all_values = np.array([])
         for j in all_files:
             directory = i + "/" + j
             z = np.loadtxt(directory + "/acc_meta.txt")
             average = np.mean(z, axis=0)
+            #median = np.median(z, axis=0)
             all_values = np.append(all_values, average)
-        x_axis = np.arange(1, len(all_values)+1) * 10
+        all_values = all_values[:19]
+        x_axis = all_files_int
+        x_axis = x_axis[:19]
         plt.plot(x_axis, all_values, label=labels[index], color=colors[index])
         #plt.fill_between(x_axis, 0, all_values, alpha=0.5, facecolor=colors[index])
         plt.scatter(x_axis, all_values, color=colors[index])
     plt.legend()
-    plt.title("Average Accuracy Per Training Images Per Class for 3 Chemical Trained RCNs")
+    plt.title("Average Accuracy Per Training Images Per Class for Trained RCNs")
     plt.xlabel("Training Images Per Class")
     plt.xlim(0, 200)
     plt.ylabel("Accuracy")
     plt.tight_layout()
     save_dir = os.getcwd() + "/graphs/"
-    plt.savefig(save_dir + "runner_graph.png")
+    plt.savefig(save_dir + "runner_rosenbaum.png")
     plt.show()
 
